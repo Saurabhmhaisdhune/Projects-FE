dbf5ac1309451938fb99433c1366f1a3cea6d508
Saurabh Lahanu Mhaisdhune <103380774+Saurabhmhaisdhune@users.noreply.github.com>
2023-04-27 11:16:15 +0530

    Update Header.js
    final updates

--- app/src/Header.js
+++ app/src/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./header.css";
 
-export default function Header(){
+export default function Header({count}){
 
     return (
         <div className="header">
             <h1 className="title">My Shopping App</h1>
+            <h1 className="title">{count} Cart</h1>
             <h1 className="userinfo">saurabh mhaisdhune</h1>
         </div>
     )
